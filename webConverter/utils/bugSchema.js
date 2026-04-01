96c58062a2612d8259237a013a7c1f268e97f08c
Andrew <eardley.andrew1@gmail.com>
2026-04-01 09:28:36 -0600

    remove web converter and move it fully to a separate project

--- webConverter/utils/bugSchema.js
+++
@@ -1,5 +0,0 @@
-let failureModes = ["get project", "serial connect", "communicate program", "flash binary", "success"];
-
-let reportTypes = ["auto", "user"];
-
-let assetTypes = ["project id", "program data", "vm log", "vm frames", "written report"];
